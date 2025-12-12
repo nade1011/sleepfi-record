660c9ebd37bd483f1692e3600e4f777e031181c4
なでちゃん <no.1011.cs@gmail.com>
2025-12-12 12:33:55 +0900

    feat: lock body scroll when mobile nav is open

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -4,17 +4,25 @@ document.addEventListener('DOMContentLoaded', () => {
     const spNav = document.getElementById('sp-nav');
     const closeBtn = document.getElementById('close-btn');
     
+    let sclollY = 0;
 
     openBtn.addEventListener('click', () => {
+      sclollY = window.scrollY;
          spNav.dataset.status = "open";
          body.dataset.menu = "open";
+         body.style.position = "fixed";
+         body.style.top = '-${sclollY}px';
+         body.style.width = '100%';
         
     });
 
     closeBtn.addEventListener('click', () => {
           spNav.dataset.status = "close";
-            body.dataset.menu = "close";
-         
+          body.dataset.menu = "close";
+          body.style.position = "";
+          body.style.top = '';
+          body.style.width = '';
+          window.scrollTo(0, sclollY);
     });
 
 });
